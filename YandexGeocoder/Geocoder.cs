ad9302254ba0dd3a656319c90444a18258242bd7
A1essandro <alexander.yermakov.1990@gmail.com>
2018-09-13 10:53:19 +0400

    Add DirectServiceProvider

--- YandexGeocoder/Geocoder.cs
+++ YandexGeocoder/Geocoder.cs
@@ -2,47 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
 using IRTech.YandexGeocoder.CacheProvider;
+using YandexGeocoder.ServiceProvider;
 
 namespace IRTech.YandexGeocoder
 {
     public class Geocoder : IDisposable
     {
-
-        private static readonly string BaseUrl = "http://geocode-maps.yandex.ru/1.x/?format=json&geocode=";
-
-        private int _requestCount = 0;
-        private readonly HttpClient _client;
+        private readonly DirectServiceProvider _directService;
         private readonly ICacheProvider _cacheProvider;
-        private readonly bool _hasCacheProvider;
-        private readonly FailureStrategy _failureStrategy;
 
-        private readonly object _counterLock = new object();
-        private readonly SemaphoreSlim _cacheLock = new SemaphoreSlim(1, 1);
-
-        public int RequestCount => _requestCount;
+        public int RequestCount => _directService.RequestCount;
 
         public ICacheProvider СacheProvider => _cacheProvider;
 
         public Geocoder(ICacheProvider cacheProvider = null, FailureStrategy failureStrategy = FailureStrategy.ReturnDefault)
         {
-            _client = new HttpClient();
             _cacheProvider = cacheProvider ?? new DummyCacheProvider();
-            _hasCacheProvider = _cacheProvider != null;
-            _failureStrategy = failureStrategy;
+            _directService = new DirectServiceProvider(_cacheProvider, failureStrategy);
         }
 
         public async Task<IEnumerable<GeoPoint>> GetPoints(string address)
         {
-            return await _getPoints(address).ConfigureAwait(false);
+            return await _directService.GetPoints(address).ConfigureAwait(false);
         }
 
         public async Task<GeoPoint> GetPoint(string address)
         {
-            var collection = await _getPoints(address).ConfigureAwait(false);
+            var collection = await GetPoints(address).ConfigureAwait(false);
             if (collection.Count() == 0)
             {
                 return null;
@@ -53,103 +41,37 @@ namespace IRTech.YandexGeocoder
 
         public async Task<IDictionary<string, GeoPoint>> GetPointByAddresses(IEnumerable<string> addresses)
         {
-            var rawResult = await _getPointsByAddressList(addresses.Distinct()).ConfigureAwait(false);
+            var rawResult = await _directService.GetPointsByAddressList(addresses.Distinct()).ConfigureAwait(false);
             return rawResult.ToDictionary(x => x.Key, x => x.Value.First());
         }
 
         public async Task<IDictionary<string, IEnumerable<GeoPoint>>> GetPointsByAddresses(IEnumerable<string> addresses)
         {
-            var rawResult = await _getPointsByAddressList(addresses.Distinct()).ConfigureAwait(false);
+            var rawResult = await _directService.GetPointsByAddressList(addresses.Distinct()).ConfigureAwait(false);
             return rawResult.ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        #region IDisposable Support
+        
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _directService.Dispose();
+                }
+                disposedValue = true;
+            }
         }
 
         public void Dispose()
         {
-            _client.Dispose();
+            Dispose(true);
         }
-
-        #region private
-
-        private async Task<IEnumerable<GeoPoint>> _getPoints(string address)
-        {
-            if (_cacheProvider.ContainsAddress(address))
-            {
-                return _cacheProvider.Get(address);
-            }
-
-            await _cacheLock.WaitAsync().ConfigureAwait(false);
-            try
-            {
-                if (_cacheProvider.ContainsAddress(address))
-                {
-                    return _cacheProvider.Get(address);
-                }
-
-                var jsonObject = await _getResponseJObject(address).ConfigureAwait(false);
-                try
-                {
-                    var rawPoints = _parseJObjectCommon(jsonObject);
-                    if (_failureStrategy == FailureStrategy.ThrowException && rawPoints.Count() == 0)
-                    {
-                        throw new Exception("Empty result");
-                    }
-                    return rawPoints.Select(x => new GeoPoint(_parsePos(x)));
-                }
-                catch (Exception ex)
-                {
-                    switch (_failureStrategy)
-                    {
-                        case FailureStrategy.ReturnDefault:
-                            return new GeoPoint[0];
-                        case FailureStrategy.ThrowException:
-                        default:
-                            throw new Exception("JSON parse error", ex);
-                    }
-                }
-            }
-            finally
-            {
-                _cacheLock.Release();
-            }
-        }
-
-        private async Task<IEnumerable<KeyValuePair<string, IEnumerable<GeoPoint>>>> _getPointsByAddressList(IEnumerable<string> addresses)
-        {
-            var emptyValues = addresses.Where(x => !_cacheProvider.ContainsAddress(x));
-            var fromCacheTask = Task.Run(() => _cacheProvider.Get(addresses.Where(x => _cacheProvider.ContainsAddress(x))));
-
-            var fillTasks = emptyValues.AsParallel()
-                .Select(async x => new KeyValuePair<string, IEnumerable<GeoPoint>>(x, await _getPoints(x).ConfigureAwait(false)));
-
-            await Task.WhenAll(fillTasks).ConfigureAwait(false);
-            var filled = fillTasks.Select(x => x.Result);
-            _cacheProvider.Set(filled);
-
-            var fromCache = await fromCacheTask.ConfigureAwait(false);
-            return fromCache.Concat(filled);
-        }
-
-        private async Task<JObject> _getResponseJObject(string address)
-        {
-            var jsonStringTask = _client.GetStringAsync(BaseUrl + address);
-            lock (_counterLock)
-            {
-                _requestCount++;
-            }
-
-            return JObject.Parse(await jsonStringTask.ConfigureAwait(false));
-        }
-
-        private static JToken _parseJObjectCommon(JObject jObject)
-        {
-            return jObject["response"]["GeoObjectCollection"]["featureMember"];
-        }
-
-        private static string _parsePos(JToken featureMember)
-        {
-            return (string)featureMember["GeoObject"]["Point"]["pos"];
-        }
-
+        
         #endregion
 
     }
---
+++ YandexGeocoder/ServiceProvider/DirectServiceProvider.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using IRTech.YandexGeocoder;
+using IRTech.YandexGeocoder.CacheProvider;
+using Newtonsoft.Json.Linq;
+
+namespace YandexGeocoder.ServiceProvider
+{
+    internal class DirectServiceProvider : IDisposable
+    {
+
+        private static readonly string BaseUrl = "http://geocode-maps.yandex.ru/1.x/?format=json&geocode=";
+
+        private readonly HttpClient _client;
+        private readonly FailureStrategy _failureStrategy;
+        private readonly ICacheProvider _cacheProvider;
+        private readonly object _counterLock = new object();
+        private readonly SemaphoreSlim _cacheLock = new SemaphoreSlim(1, 1);
+        private int _requestCount = 0;
+
+        public int RequestCount => _requestCount;
+
+        internal DirectServiceProvider(ICacheProvider cacheProvider, FailureStrategy failureStrategy)
+        {
+            _client = new HttpClient();
+            _failureStrategy = failureStrategy;
+            _cacheProvider = cacheProvider;
+        }
+
+        public async Task<IEnumerable<GeoPoint>> GetPoints(string address)
+        {
+            if (_cacheProvider.ContainsAddress(address))
+            {
+                return _cacheProvider.Get(address);
+            }
+
+            await _cacheLock.WaitAsync().ConfigureAwait(false);
+            try
+            {
+                if (_cacheProvider.ContainsAddress(address))
+                {
+                    return _cacheProvider.Get(address);
+                }
+
+                var jsonObject = await _getResponseJObject(address).ConfigureAwait(false);
+                try
+                {
+                    var rawPoints = _parseJObjectCommon(jsonObject);
+                    if (_failureStrategy == FailureStrategy.ThrowException && rawPoints.Count() == 0)
+                    {
+                        throw new Exception("Empty result");
+                    }
+                    return rawPoints.Select(x => new GeoPoint(_parsePos(x)));
+                }
+                catch (Exception ex)
+                {
+                    switch (_failureStrategy)
+                    {
+                        case FailureStrategy.ReturnDefault:
+                            return new GeoPoint[0];
+                        case FailureStrategy.ThrowException:
+                        default:
+                            throw new Exception("JSON parse error", ex);
+                    }
+                }
+            }
+            finally
+            {
+                _cacheLock.Release();
+            }
+        }
+
+        public async Task<IEnumerable<KeyValuePair<string, IEnumerable<GeoPoint>>>> GetPointsByAddressList(IEnumerable<string> addresses)
+        {
+            var emptyValues = addresses.Where(x => !_cacheProvider.ContainsAddress(x));
+            var fromCacheTask = Task.Run(() => _cacheProvider.Get(addresses.Where(x => _cacheProvider.ContainsAddress(x))));
+
+            var fillTasks = emptyValues
+                .Select(async x => new KeyValuePair<string, IEnumerable<GeoPoint>>(x, await GetPoints(x).ConfigureAwait(false)));
+
+            await Task.WhenAll(fillTasks).ConfigureAwait(false);
+            var filled = fillTasks.Select(x => x.Result);
+            _cacheProvider.Set(filled);
+
+            var fromCache = await fromCacheTask.ConfigureAwait(false);
+            return fromCache.Concat(filled);
+        }
+
+        private async Task<JObject> _getResponseJObject(string address)
+        {
+            var jsonStringTask = _client.GetStringAsync(BaseUrl + address);
+            lock (_counterLock)
+            {
+                _requestCount++;
+            }
+
+            return JObject.Parse(await jsonStringTask.ConfigureAwait(false));
+        }
+
+        private static JToken _parseJObjectCommon(JObject jObject)
+        {
+            return jObject["response"]["GeoObjectCollection"]["featureMember"];
+        }
+
+        private static string _parsePos(JToken featureMember)
+        {
+            return (string)featureMember["GeoObject"]["Point"]["pos"];
+        }
+
+        #region IDisposable Support
+        
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _client.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        
+        #endregion
+
+    }
+}
